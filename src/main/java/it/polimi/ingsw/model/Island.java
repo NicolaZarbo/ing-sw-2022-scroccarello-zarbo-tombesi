f07b182aa70bd3caa73586e805c9a66436e4f397
sinnanzi <100701876+sinnanzi@users.noreply.github.com>
2022-03-27 20:17:53 +0200

    correzione errori di commit

--- src/main/java/it/polimi/ingsw/model/Island.java
+++ src/main/java/it/polimi/ingsw/model/Island.java
@@ -1,54 +1,46 @@
 package it.polimi.ingsw.model;
 import java.util.ArrayList;
-import java.util.List;
-
 public class Island {
     private int ID;
     private ArrayList<Student> students;
-    private ArrayList<Tower> towers;
-    private int islandSize;
+    private ArrayList<Tower> tower;
 
-    public Island(int id){
-        this.ID=id;
-        this.students=new ArrayList<Student>();
-        this.towers=new ArrayList<Tower>();
-        this.islandSize=0;
+    public Island(int id) {
+        this.ID = id;
+        this.students = new ArrayList<Student>();
+        this.tower = new ArrayList<Tower>();
     }
-    public int getID() {return this.ID;}
+
+    public int getID() {
+        return this.ID;
+    }
 
     //ritorna tutte le torri sull'isola attenzione ai null!(ovvero se viene chiamato e l'isola non ha torri)
-    public ArrayList<Tower> getTower(){
-        return this.towers;
+    public ArrayList<Tower> getTower() {
+        return this.tower;
     }
+
     //metodo per aggiungere un singolo studente su un'isola (quando il giocatore di sua sponte lo aggiunge)
-    public void addStudent(Student t){
+    public void addStudent(Student t) {
         students.add(t);
     }
 
-    public ArrayList<Student> getStudents(){
+    //metodo per mergiare gli studenti di due isole
+    public void addAllStudents(ArrayList<Student> s) {
+        this.students.addAll(s);
+    }
+
+    public ArrayList<Student> getStudents() {
         return this.students;
     }
 
     //metodo per settare la torre su un'isola che non ce l'ha
-    public void setTower(Tower t){
-        this.towers.add(t);
+    public void setTower(Tower t) {
+        this.tower.add(t);
     }
 
-    private void mergeIslands(Island island, List<Island> isl) {
-        this.islandSize++;
-        for (int i=0;i<isl.size();) {
-            if(isl.get(i).getID()==island.getID()) {
-                isl.remove(i);
-                break;
-            }
-        }
-        for (int i=0;i<island.students.size();i++){
-            this.students.add(island.students.get(i));
-        }
-        for (int i=0;i<island.towers.size();i++){
-            this.towers.add(island.towers.get(i));
-        }
+    //metodo per mergiare le torri di due isole
+    public void addAllTowers(ArrayList<Tower> t) {
+        this.tower.addAll(t);
     }
-
-
 }
