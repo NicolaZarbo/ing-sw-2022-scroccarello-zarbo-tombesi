14deba91bbf41972503563eb6004a4d93f238942
lucatombesi99 <68461003+lucatombesi99@users.noreply.github.com>
2022-04-02 17:09:30 +0200

    01/04/2022 18:48 aggiunto main

--- src/main/java/it/polimi/ingsw/controller/Main.java
+++ src/main/java/it/polimi/ingsw/controller/Main.java
@@ -1,29 +1,43 @@
 package it.polimi.ingsw.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    public static String[] players;
+    public static boolean[][] assistantsplayed;
+    public static boolean[][] towersplayed;
+    public static ArrayList<int[]> clouds;
+    public static int mother;        //il suo valore indica la posizione (ha il valore dell'isola)
+    public static  List<Integer> islands;
+    public static int bag; //il suo valore indica il numero di pedine rimaste nel sacchetto
+
+
     public static void main(String[] args) {
-        String[] players;
-        int[][] assistants;
         boolean win=false;
-        int mother;//il suo valore indica la posizione (ha il valore dell'isola)
-        List<Integer> islands;
-        int bag; //il suo valore indica il numero di pedine rimaste nel sacchetto
-        ArrayList<int[]> clouds=new ArrayList<int[]>();
+        boolean[][] towersplayed;
+        bag=120;
+        clouds=new ArrayList<int[]>();
         Scanner sc = new Scanner(System.in);
         int numPlayers= sc.nextInt();
         players=new String[numPlayers];
-        assistants=new int[numPlayers][10];
+        assistantsplayed =new boolean[numPlayers][10];
+        for (boolean[] booleans : assistantsplayed) Arrays.fill(booleans, false);
+
+        if(numPlayers==2)
+            towersplayed=new boolean[numPlayers][8];
+        else if(numPlayers==3)
+            towersplayed=new boolean[numPlayers][6];
+        else
+            towersplayed=new boolean[2][8];
+
+        for(int i=0;i<numPlayers;i++)
+            Arrays.fill(towersplayed[i], false);
+
+
         sc.nextLine();
         for(int i=0;i<numPlayers;i++) {
             System.out.println("Inserire il nick del player "+i);
             players[i]=sc.nextLine();
-            for (int j = 0; j < 10; j++)
-                assistants[i][j] = 1;
         }
             for(int i=0;i<numPlayers;i++)
                 clouds.add(new int[3]);
@@ -33,7 +47,10 @@ public class Main {
         //mettere studente su ogni isola (ricorda nel bag all'inizio sono solo 10 pedine,2 per ogni colore
         // opposta all'isola di madre natura c'è un'isola senza studente)
         int firstRoundPlayer=rand.nextInt(numPlayers);
-      /*  while(!win){
+        while(!someoneWon()){
+            if(bag>=6){
+
+            }
             //pesca 3 students dal bag
             //ogni giocatore sceglie un'assistente da giocare, si procede in ordine orario
             //fase azione di ogni giocatore
@@ -44,9 +61,42 @@ public class Main {
 
 
         }
-        */
+
 
         System.out.println("bruh");
 
+    }
+
+
+    public static boolean win(int player){
+        boolean b=true;
+        for(int i=0;i<towersplayed[player].length;i++)
+            if(!towersplayed[player][i])
+                b=false;
+
+        return b;
+    }
+    public static boolean someoneWon(){
+       boolean b=false;
+       boolean found=false;
+       int count=10;
+        for(int i=0;i<towersplayed.length;i++)
+            if(win(i))
+                b=true;
+            if(islands.size()==3)
+                b=true;
+
+        for (boolean[] booleans : assistantsplayed) {
+            for (boolean aBoolean : booleans) {
+                if (aBoolean)
+                    count--;
+            }
+            if (count == 0 && !found) {
+                b = true;
+                found = true;
+            }
+        }
+
+            return b;
     }
 }
