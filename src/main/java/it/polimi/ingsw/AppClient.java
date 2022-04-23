9012440d00a3a25016a190064ab40b454268b669
lucatombesi99 <68461003+lucatombesi99@users.noreply.github.com>
2022-04-23 17:58:59 +0200

    01/04/2022 18:48 aggiunto main

---
+++ src/main/java/it/polimi/ingsw/AppClient.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.client.Client;
+
+import java.io.IOException;
+
+public class AppClient {
+    public static void main(String[] args){
+        Client client = new Client("127.0.0.1", 12345);
+        try{
+            client.run();
+        }catch (IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
