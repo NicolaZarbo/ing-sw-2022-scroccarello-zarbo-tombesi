4ee6b6f698d6dee9950d882eb3efdd4696d27b54
sinnanzi <100701876+sinnanzi@users.noreply.github.com>
2022-05-31 15:30:04 +0200

    board3 scene almost ended

--- src/main/java/it/polimi/ingsw/client/GUI/Scenes/BoardScene3Controller.java
+++ src/main/java/it/polimi/ingsw/client/GUI/Scenes/BoardScene3Controller.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.client.GUI.Scenes;
 
 
+import it.polimi.ingsw.view.simplifiedobjects.SimplifiedPlayer;
 import javafx.scene.Node;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -39,17 +40,48 @@ public class BoardScene3Controller extends BoardSceneController{
 
     public BoardScene3Controller(){
         super();
+        this.diningRoom0=new ArrayList<>();
+        this.diningRoom1=new ArrayList<>();
+        this.diningRoom2=new ArrayList<>();
     }
 
     @Override
     public void initialize() {
         for(int i=0;i<3;i++){
             buildEntrance(i);
-            setEntrance(entrance0List,i+1);
+            buildHall(i);
+            buildTable(i);
+            buildTowers(i);
         }
+        int other1=-1;
+        int other2=-1;
+        for(SimplifiedPlayer p : gui.getGame().getPlayers()){
+            if(p!=gui.getGame().getPersonalPlayer()){
+                if(other1==-1){
+                    other1=p.getId();
+                }
+                else{
+                    other2=p.getId();
+                }
+            }
+        }
+        setEntrance(entrance0List,gui.getGame().getPersonalPlayer().getId()+1);
+        setEntrance(entrance1List,other1+1);
+        setEntrance(entrance2List,other2+1);
 
+        setHall(diningRoom0,gui.getGame().getPersonalPlayer().getId()+1);
+        setHall(diningRoom1,other1+1);
+        setHall(diningRoom2,other2+1);
 
+        setProfessors(table0,gui.getGame().getPersonalPlayer().getId()+1);
+        setProfessors(table1,other1+1);
+        setProfessors(table2,other2+1);
+
+        setTowers(towers0,gui.getGame().getPersonalPlayer().getId()+1);
+        setTowers(towers1,other1+1);
+        setTowers(towers2,other2+1);
     }
+
     private void buildEntrance(int player){
        switch(player){
            case 0->{
@@ -75,18 +107,20 @@ public class BoardScene3Controller extends BoardSceneController{
     }
     private void buildHall(int player){
         ArrayList<Circle> diningRow = new ArrayList<>();
-        Pane diningReference;
-        ArrayList<ArrayList<Circle>> diningContainer=null;
+        Pane diningReference=null;
+        ArrayList<ArrayList<Circle>> diningContainer=new ArrayList<>();
         switch(player) {
             case 0-> {
                 diningContainer=diningRoom0;
-                diningReference=dining_1;
+                diningReference=yourhall;
             }
-            case 1->{}
-
+            case 1->{
+                diningContainer=diningRoom1;
+                diningReference=other1hall;
+            }
             case 2-> {
                 diningContainer = diningRoom2;
-                diningReference = yourhall;
+                diningReference = other2hall;
             }
         }
 
@@ -98,4 +132,51 @@ public class BoardScene3Controller extends BoardSceneController{
             diningRow=new ArrayList<>();
         }
     }
+    private void buildTable(int player){
+        switch(player) {
+            case 0 -> {
+                table0 = new ArrayList<>();
+                for (Node teacher : yourtable.getChildren()) {
+                    table0.add((Polygon) teacher);
+                }
+            }
+
+            case 1->{
+                    table1 = new ArrayList<>();
+                    for (Node teacher : other1table.getChildren()) {
+                        table1.add((Polygon) teacher);
+                    }
+            }
+            case 2->{
+                table2 = new ArrayList<>();
+                for (Node teacher : other2table.getChildren()) {
+                    table2.add((Polygon) teacher);
+                }
+            }
+        }
+    }
+
+    private void buildTowers(int player){
+        switch(player){
+            case 0->{
+            towers0=new ArrayList<>();
+            for (Node tower:yourtowers.getChildren()) {
+                towers0.add((ImageView) tower);
+            }
+        }
+            case 1->{
+                towers1=new ArrayList<>();
+                for (Node tower:other1towers.getChildren()) {
+                    towers1.add((ImageView) tower);
+                }
+        }
+            case 2->{
+                towers2=new ArrayList<>();
+                for (Node tower:other2towers.getChildren()) {
+                    towers2.add((ImageView) tower);
+                }
+            }
+    }
+    }
+
 }
