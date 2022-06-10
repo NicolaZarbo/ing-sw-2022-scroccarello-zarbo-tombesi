52040094f7be657441fd45179329dec9c468e98d
lucatombesi99 <68461003+lucatombesi99@users.noreply.github.com>
2022-06-10 19:48:44 +0200

    Update SelectCharacterController.java
    gghgh

--- src/main/java/it/polimi/ingsw/client/GUI/Scenes/SelectCharacterController.java
+++ src/main/java/it/polimi/ingsw/client/GUI/Scenes/SelectCharacterController.java
@@ -1,6 +1,9 @@
 package it.polimi.ingsw.client.GUI.Scenes;
 
+import javafx.scene.Node;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
@@ -8,9 +11,17 @@ import java.util.ArrayList;
 
 public class SelectCharacterController extends SceneController{
 
-    public Pane general;
+    public Pane CharacterContainer;
+
+    public Pane studentCharacterContainer;
+
+    public Pane coinImageContainer;
+
+    public Pane valueCharacterContainer;
 
     private ArrayList<Rectangle> characters;
+
+    private ArrayList<Circle> coins;
 
     private ArrayList<Circle> character1stud;
 
@@ -23,8 +34,35 @@ public class SelectCharacterController extends SceneController{
 
     }
 
-    public void initCharacters(){
+    private void initCharacters(){
         characters=new ArrayList<>();
-            ArrayList<Rectangle> chars=new ArrayList<>();
+        coins=new ArrayList<>();
+        for(int i=0;i<8;i++){
+            characters.add((Rectangle)CharacterContainer.getChildren() );
+            coins.add((Circle)coinImageContainer.getChildren() );
+
+            coinPlace.setFill(new ImagePattern(new Image("images/simple_elements/coin.png")));
+            charactercost.setText(view.getPersonalPlayer().getCoin()+"");
+        }
+        for(int i=0;i<coins.size();i++)
+            coins.get(i).setFill(new ImagePattern(new Image("images/simple_elements/coin.png")));
+
+    }
+
+
+
+    private void initStudCharacters(){
+        character1stud=new ArrayList<>();
+        character7stud=new ArrayList<>();
+        character11stud=new ArrayList<>();
+        for (int i=0;i<14;i++) {
+            if(i<4) {
+                character1stud.add((Circle) studentCharacterContainer.getChildren());
+            }else if(i>=4 && i<10){
+                character7stud.add((Circle) studentCharacterContainer.getChildren());
+            }else{
+                character11stud.add((Circle) studentCharacterContainer.getChildren());
+            }
+        }
     }
 }
